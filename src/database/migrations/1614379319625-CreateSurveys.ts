222bf5d8df60244ffa2a966c565f9942b243c09c
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-02-27 01:07:21 -0300

    Add Create Survey migration

---
+++ src/database/migrations/1614379319625-CreateSurveys.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSurveys1614379319625 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "Surveys",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("Surveys");
+    }
+
+}
