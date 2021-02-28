3690ba89d8cfca6725e46a16fce794905e23a12f
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-02-28 01:18:40 -0300

    Add validators - database type

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,10 +1,34 @@
 import request from "supertest";
 import { app } from "../app";
 
+import createConnection from "../database"
+
 describe("Users", () => {
-    request(app).post("/users")
-        .send({
-            email: "user@example.com",
-            name: "User Example"
+    // Antes de qualquer execução: rodar as migrations
+    // Adição de async e await por se tratarem de promises
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example1.com",
+            name: "User Example1"
         })
-})
+
+        expect(response.status).toBe(201);
+
+    });
+
+    it("Should not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example1.com",
+            name: "User Example1"
+        })
+
+        expect(response.status).toBe(400);
+
+    });
+
+});
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,18 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+export default async (): Promise<Connection> => {
+    // verificar se trata-se de teste ou de produção
+    const defaultOptions = await getConnectionOptions();
+    return createConnection(
+        // Sobrescrever database
+        Object.assign(defaultOptions, {
+            // acessando variável de ambiente NODE_ENV
+            database:
+                // Se for um teste, criar banco de dados específico, caso contrário, seguir com o banco inicial
+                process.env.NODE_ENV === "test"
+                    ? "./src/database/database.test.sqlite"
+                    : defaultOptions.database
+        })
+    );
+};
 
