a9a5635ddd3a7d8981ce07a8f31f644c19a6a93b
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-02-27 22:23:54 -0300

    Add second test as experiment

--- src/__tests__/First.test.ts
+++ src/__tests__/First.test.ts
@@ -3,5 +3,10 @@
 describe("First", () => {
     it("Deve ser possível somar dois números", () => {
         expect(2 + 2).toBe(4)
-    })
+    });
+
+    it("Deve ser possível somar dois números", () => {
+        expect(2 + 2).not.toBe(5)
+    });
+
 });
